65c2292202f86a6c9d61e57106c9a73af09919f4
AnastasiaSvyat <64147723+AnastasiaSvyat@users.noreply.github.com>
2021-07-01 16:12:14 +0300

    01/07

--- src/app/admin/add-user/add-user.component.ts
+++ src/app/admin/add-user/add-user.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { AdminComponent } from '../admin.component';
 
 @Component({
   selector: 'app-add-user',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddUserComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<AddUserComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: AdminComponent
+
+  ) { }
 
   ngOnInit(): void {
+  }
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
