5d478f03acd5624eb5e01e13c439f9f5f9b2eacc
AnastasiaSvyat <64147723+AnastasiaSvyat@users.noreply.github.com>
2022-01-26 14:40:33 +0200

    fix bugs

--- src/app/user/dashboard-user/dashboard-user.component.ts
+++ src/app/user/dashboard-user/dashboard-user.component.ts
@@ -187,8 +187,10 @@ export class DashboardUserComponent implements OnInit {
         this.emoloyeeService.GetEmplBirthMonth()
           .subscribe((result) => {
             this.birthMonthList = result;
+            console.log(this.birthMonthList);
             this.eventMonthList.forEach((element: any) => {
               this.birthMonthList.push(element);
+              console.log(this.birthMonthList);
             });
           });
       });
